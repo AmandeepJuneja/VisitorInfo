d77ad97b892688595e39f35baff0044a385dac86
sighosh <siddhartha.ghosh@in.ibm.com>
2015-03-13 21:13:02 +0530

    * UI Changes as per design specs, nearly completed.

--- WebContent/index.js
+++ WebContent/index.js
@@ -2,11 +2,49 @@
  * 
  */
 
+/* GLOBALS */
+var  dialog = $('#createVisitDialog').dialog({
+	autoOpen: false,
+	height: 600,
+	width: 800,
+	modal: true,
+	hide: 'clip',
+	show: 'clip',
+	buttons: {
+		'Create Visit': createVisitRecord,
+		'Cancel': function() {
+			dialog.dialog('close');
+		}
+	},
+	close: function() {
+		form[0].reset();
+	}
+});
+
+var  form = dialog.find('form').on('submit', function(evt) {
+	evt.preventDefault();
+	createVisitDialog();
+});
+
+function openVisitDialog() {
+	dialog.dialog('open');
+}
+
+function createVisitRecord() {
+	
+}
 
 $(function() {
 	// bootstrap bill :)
 	$('#messagesDiv').hide('slide');
-	$('#createVisitBtn').button();
+	$('#createVisitBtn').button().click(openVisitDialog);
 	$('#searchVisitBtn').button();
-	
+	$('#addVisitorBtn').button();
+	$('#removeVisitorsBtn').button();
+	$('#itiAddBtn').button();
+	$('#itiRemoveBtn').button();
+	$('#ldrPartAdd').button();
+	$('#ldrPartRemove').button();
+	$('#createVisitTabs').tabs();
+	$('#visitTypeRadio').buttonset();
 });
