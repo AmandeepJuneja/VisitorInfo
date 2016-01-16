924f7cf48d2e8e9ec49f4c58f140951497327656
sidghosh <siddhartha.ghosh.in@outlook.com>
2016-01-16 21:18:21 +0530

    * added cloudant test code.

--- src/com/ibm/vis/resources/TestCloudant.java
+++ src/com/ibm/vis/resources/TestCloudant.java
@@ -6,8 +6,16 @@
 
 package com.ibm.vis.resources;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
+import org.json.JSONObject;
+
+import com.cloudant.client.api.ClientBuilder;
+import com.cloudant.client.api.CloudantClient;
 
 /**
  * The purpose of this class is to test out the Cloudant NoSQL database in BlueMix
@@ -19,8 +27,40 @@ public class TestCloudant {
 	@GET
 	public String helloCloudant() {
 		StringBuffer messages = new StringBuffer();
+		messages.append("reading configurations...");
 		
-		messages.append("testing cloudant...");
+		String vcap = System.getenv("VCAP_SERVICES");
+		if ( vcap == null ) {
+			messages.append("[FAILED]\n");
+			messages.append("null or empty configurations");
+			return messages.toString();
+		} else {
+			messages.append("[OK]\n");
+		}
+
+		
+		JSONObject serviceObj = new JSONObject(vcap);
+		String cloudantURL = serviceObj.getJSONArray("cloudantNoSQLDB").getJSONObject(0).getJSONObject("credentials").getString("url");
+		String userid = serviceObj.getJSONArray("cloudantNoSQLDB").getJSONObject(0).getJSONObject("credentials").getString("username");
+		String password = serviceObj.getJSONArray("cloudantNoSQLDB").getJSONObject(0).getJSONObject("credentials").getString("password");
+		
+		messages.append("creating cloudant client...");
+		
+		// Create a new CloudantClient instance for account endpoint example.cloudant.com
+		try {
+			CloudantClient client = ClientBuilder.url(new URL(cloudantURL))
+					.username(userid)
+					.password(password)
+					.build();
+			messages.append("[OK]\n");
+			messages.append("connected to cloudant couchdb server version " + client.serverVersion());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			messages.append("[FAILED]\n");
+			messages.append("encountered error: " + e.getMessage());
+		} 
+		
 		return messages.toString();
 	}
 }
