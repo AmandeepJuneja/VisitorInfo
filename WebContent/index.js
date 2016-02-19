ca738f9fa02b4db7f2c0bd91ba7344ef6a342329
sidghosh <siddhartha.ghosh.in@outlook.com>
2016-02-20 03:21:03 +0530

    * data tables minor ui modifications.

--- WebContent/index.js
+++ WebContent/index.js
@@ -49,7 +49,11 @@ var  form = dialog.find('form').on('submit', function(evt) {
 	createVisitRecord();
 });
 
-var visitRecordsTable = $('#visitRecordsTable').DataTable();
+var visitRecordsTable = $('#visitRecordsTable').DataTable({
+	'language': {
+		'search': 'Filter: '
+	}
+});
 
 /*
  * Global functions
