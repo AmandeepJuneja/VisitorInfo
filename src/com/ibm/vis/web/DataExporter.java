da0802b932f13a73a583b838680c0b45272c723f
sidghosh <siddhartha.ghosh.in@outlook.com>
2016-02-20 06:43:08 +0530

    * Full data export support.

--- src/com/ibm/vis/web/DataExporter.java
+++ src/com/ibm/vis/web/DataExporter.java
@@ -13,10 +13,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 
 import com.cloudant.client.api.Database;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.ibm.vis.resources.CloudVisit;
 import com.ibm.vis.utils.CloudDBUtil;
@@ -60,6 +64,12 @@ public class DataExporter extends HttpServlet {
 		headerRow.createCell(4).setCellValue("CBC");
 		headerRow.createCell(5).setCellValue("HOSTING MANAGER");
 		headerRow.createCell(6).setCellValue("VISIT AGENDA");
+		headerRow.createCell(7).setCellValue("VISITORS");
+		headerRow.createCell(8).setCellValue("ITINERARY");
+		headerRow.createCell(9).setCellValue("LEADERSHIP PARTICIPATION");
+		headerRow.createCell(10).setCellValue("EXECUTIVE OWNER(TCV > 10 Million)");
+		headerRow.createCell(11).setCellValue("OPPORTUNITY TCV(in $M)");
+		headerRow.createCell(12).setCellValue("DELIVERY TYPE");
 		
 		for (String docId : allDocIds) {
 			JsonObject visitObj = database.find(JsonObject.class, docId);
@@ -98,6 +108,90 @@ public class DataExporter extends HttpServlet {
 			if ( visitObj.get("visitAgenda") != null ) {
 				String visitAgenda = visitObj.get("visitAgenda").getAsString();
 				dataRow.createCell(6).setCellValue(visitAgenda);
+			}
+			
+			// NESTED FIELD -- VISITORS
+			if ( visitObj.get("visitorRecords") != null ) {
+				JsonArray visitorRecords = visitObj.get("visitorRecords").getAsJsonArray();
+				Cell cell = dataRow.createCell(7);
+				CellStyle style = workbook.createCellStyle();
+			    style.setWrapText(true);
+			    cell.setCellStyle(style);
+			    StringBuffer visitorsBuffer = new StringBuffer();
+				for ( JsonElement visitorRecordElement : visitorRecords ) {
+					String visitorName = visitorRecordElement.getAsJsonObject().get("visitorName").getAsString();
+					String visitorRole = visitorRecordElement.getAsJsonObject().get("visitorRole").getAsString();
+					String visitorPrimary = visitorRecordElement.getAsJsonObject().get("visitorPrimary").getAsString();
+					visitorsBuffer.append(visitorName + " ");
+					visitorsBuffer.append("[Role: " + visitorRole + "]");
+					if ( visitorPrimary != null && visitorPrimary.trim().length() > 0 
+							&& (visitorPrimary.equalsIgnoreCase("YES") || visitorPrimary.equalsIgnoreCase("Y") ) ) {
+						visitorsBuffer.append(" (Primary)");
+					}
+					visitorsBuffer.append("\n");
+				}
+				cell.setCellValue(visitorsBuffer.toString());
+			}
+			
+			// NESTED FIELD -- ITINERARY
+			if ( visitObj.get("itineraryRecords") != null ) {
+				JsonArray itineraryRecords = visitObj.get("itineraryRecords").getAsJsonArray();
+				Cell cell = dataRow.createCell(8);
+				CellStyle style = workbook.createCellStyle();
+			    style.setWrapText(true);
+			    cell.setCellStyle(style);
+			    StringBuffer itineraryBuffer = new StringBuffer();
+			    for ( JsonElement itineraryElement : itineraryRecords ) {
+			    	itineraryBuffer.append(itineraryElement.getAsJsonObject().get("itiLoc").getAsString());
+			    	itineraryBuffer.append(" (From: " + itineraryElement.getAsJsonObject().get("").getAsString());
+			    	itineraryBuffer.append(" To: " + itineraryElement.getAsJsonObject().get("").getAsString() + ")");
+			    	itineraryBuffer.append("\n");
+			    }
+			    cell.setCellValue(itineraryBuffer.toString());
+			}
+			
+			// NESTED FIELD -- LEADERSHIP PARTICIPATION
+			if ( visitObj.get("leadershipRecords") != null ) {
+				JsonArray leadershipRecords = visitObj.get("leadershipRecords").getAsJsonArray();
+				Cell cell = dataRow.createCell(9);
+				CellStyle style = workbook.createCellStyle();
+			    style.setWrapText(true);
+			    cell.setCellStyle(style);
+			    StringBuffer leadershipBuffer = new StringBuffer();
+			    for ( JsonElement leadershipElement : leadershipRecords ) {
+			    	leadershipBuffer.append(leadershipElement.getAsJsonObject().get("ldrLNID").getAsString());
+			    	leadershipBuffer.append(" (" + leadershipElement.getAsJsonObject().get("ldrBU").getAsString() + ")");
+			    	leadershipBuffer.append(" (" + leadershipElement.getAsJsonObject().get("ldrAttnd").getAsString() + ")");
+			    	leadershipBuffer.append(" (" + leadershipElement.getAsJsonObject().get("").getAsString() + ")");
+			    	leadershipBuffer.append(" (" + leadershipElement.getAsJsonObject().get("").getAsString() + ")");
+			    	leadershipBuffer.append("\n");
+			    }
+			    cell.setCellValue(leadershipBuffer.toString());
+			}
+			
+			if ( visitObj.get("execOwnerTCV") != null ) {
+				String execOwnerTCV = visitObj.get("execOwnerTCV").getAsString();
+				dataRow.createCell(10).setCellValue(execOwnerTCV);
+			}
+			
+			if ( visitObj.get("opportunityTCV") != null ) {
+				String opportunityTCV = visitObj.get("opportunityTCV").getAsString();
+				dataRow.createCell(11).setCellValue(opportunityTCV);
+			}
+			
+			if ( visitObj.get("deliveryTypeChoice") != null ) {
+				String deliveryTypeChoice = visitObj.get("deliveryTypeChoice").getAsString();
+				switch(deliveryTypeChoice) {
+				case "E":
+					dataRow.createCell(12).setCellValue("Existing Delivery");
+					break;
+				case "N":
+					dataRow.createCell(12).setCellValue("New Opportunity");
+					break;
+				default:
+					dataRow.createCell(12).setCellValue("Unknown");
+				}
+				
 			}
 		}
 		
